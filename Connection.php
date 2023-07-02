65d4371505cce8de139cb64984131db2fcbda0be
taegvils <101215542+taegvils@users.noreply.github.com>
2023-07-02 17:59:46 -0300

    Connection.php

--- Connection.php
+++ Connection.php
@@ -1,1 +1,23 @@
+<?php
+    class Connection {
+        private static $conn = null;
 
+        public static function getConnection() {
+
+            if(self::$conn == null) {
+                
+                $opcoes = array(
+                    //Define o charset da conexão
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                    //Define o tipo do erro como exceção
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                );
+
+                self::$conn = new PDO("mysql:host=localhost;dbname=dbpessoas", "root", "", $opcoes);
+
+                //print_r($connec);
+            }
+            return self::$conn;
+        }
+    }
+?>
