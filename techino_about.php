4f1e67789f178805498db1cc4c8e71b1862dbbf0
pedrocanu000 <130390165+pedrocanu000@users.noreply.github.com>
2023-07-02 18:53:04 -0300

    Update techino_about.php
    código completo techino_about.php

--- techino_about.php
+++ techino_about.php
@@ -1,1 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Sobre nós</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
+
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="carousel.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <style type="text/css">@font-face { font-family: Roboto; src: url("chrome-extension://mcgbeeipkmelnpldkobichboakdfaeon/css/Roboto-Regular.ttf"); }</style></
+</head>
+<body>
+  <nav class="py-2 bg-dark border-bottom">
+    <div class="container d-flex flex-wrap">
+    <ul class="nav me-auto">
+        <li class="nav-item"><a href="techino_home.php" class="nav-link link-light px-2 active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="techino_about.php" class="nav-link link-light px-2">About</a></li>
+    </ul>
+    <ul class="nav">
+        <li class="nav-item"><a href="techino_logar.php" class="nav-link text-blue fw-bold px-2">Login</a></li>
+        <li class="nav-item"><a href="techino_register.php" class="nav-link text-blue fw-bold px-2">Sign up</a></li>
+    </ul>
+    </div>
+  </nav>
+
+  <header class="py-3 mb-4 border-bottom bg-dark">
+      <div class="container d-flex flex-wrap justify-content-center">
+          <a class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-light text-decoration-none">
+              <span class="fs-4 tex">TECH-INO</span>
+          </a>
+          <form class="col-12 col-lg-auto mb-3 mb-lg-0">
+              <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+          </form>
+      </div>
+  </header>
+
+    <main role="main" >
+
+        <div id="carouselExampleDark" class="carousel carousel-dark slide">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-bs-interval="2000">
+                <img src="numero2.png" class="d-block w-100" alt="">
+                <div class="carousel-caption d-none d-md-block">
+                  
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="2000">
+                <img src="banner2.png" class="d-block w-100" alt="">
+                <div class="carousel-caption d-none d-md-block">
+                  
+                </div>
+              </div>
+              <div class="carousel-item" data-bs-interval="2000">
+                <img src="banner3.png" class="d-block w-100" alt="">
+                <div class="carousel-caption d-none d-md-block">
+                  
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <br><br>
+
+        <div class="container marketing">
+
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+              <div class="col-lg-4">
+                <img class="rounded-circle" src="gif1.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Número comercial</h2>
+                <p>Um número comercial é essencial para o sucesso e eficiência das comunicações de nossa empresa. Ele transmite profissionalismo, 
+                    separa as interações pessoais das profissionais, permite escalabilidade, facilita o gerenciamento centralizado, possibilita 
+                    a análise de dados, fortalece o marketing e branding. Em resumo, um número comercial é uma ferramenta indispensável 
+                    para nossa empresa, contribuindo para sua reputação, eficiência e crescimento a longo prazo.
+                    </p>
+                <p><a class="btn btn-secondary" href="#" role="button">Ver detalhes »</a></p>
+              </div><!-- /.col-lg-4 -->
+              <div class="col-lg-4">
+                <img class="rounded-circle" src="gif2.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Benefícios da Tecnologia Interativa </h2>
+                <p>A tecnologia interativa desempenha um papel importante em nossas vidas, proporcionando benefícios como engajamento do usuário, aprendizado aprimorado, 
+                    melhoria na comunicação, experiências imersivas, personalização e adaptação, e estímulo à inovação e criatividade. Em resumo, a tecnologia interativa
+                    transforma as experiências, tornando-as mais envolventes, personalizadas e inovadoras.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">Ver detalhes»</a></p>
+              </div><!-- /.col-lg-4 -->
+              <div class="col-lg-4">
+                <img class="rounded-circle" src="gif3.png" alt="Generic placeholder image" width="140" height="140">
+                <h2>Como a Tecnologia Está Transformando a Educação</h2>
+                <p>A tecnologia está transformando a educação de maneiras significativas. Ela proporciona acesso a recursos educacionais online, promove a aprendizagem personalizada, 
+                    facilita a colaboração e a comunicação entre alunos e professores, e permite experiências de aprendizado imersivas. Com a tecnologia, a educação se torna mais interativa, 
+                    envolvente e acessível, preparando os alunos para enfrentar os desafios do mundo moderno.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">Ver detalhes»</a></p>
+              </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+    
+    
+            <!-- START THE FEATURETTES -->
+    
+            <hr class="featurette-divider">
+    
+            <div class="row featurette">
+              <div class="col-md-7">
+                <h2 class="featurette-heading">A TECH-INO é uma nova empresa que visa a comercialização de
+                    componentes eletrônicos para Arduino, Robótica, IoT e Automação Residencial por todo o Brasil. <span class="text-muted"></span></h2> <br>
+                <p class="lead">Aqui você encontra tudo que precisa para criar seus projetos, com o melhor preço e Suporte Técnico gratuito.
+                    De makers, para makers: exploramos junto com você todas as possibilidades do mundo da eletrônica,
+                    por isso, desenvolvemos diversos Kits Didáticos com Apostilas 100% gratuitas.</p>
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto rounded img-thumbnail" data-src="holder.js/500x500/auto" alt="500x500" style="width: 100%; height: 100%;" src="robotica1.png" data-holder-rendered="true">
+              </div>
+            </div>
+    
+            <hr class="featurette-divider">
+    
+            <div class="row featurette">
+              <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading">Da estrutura até os processos, a TECH-INO preza em entregar a melhor experiência para o cliente, desde a navegação em nosso site, e principalmente após a entrega do pedido. <span class="text-muted"></span></h2> <br>
+                <p class="lead">Além do site principal, atuamos também em diversos Marketplaces, como Americanas, Submarino, Mercado Livre, Magalu, entre outros.</p>
+              </div>
+              <div class="col-md-5 order-md-1">
+                <img class="featurette-image img-fluid mx-auto rounded img-thumbnail" data-src="holder.js/500x500/auto" alt="500x500" style="width: 100%; height: 100%;" src="robotica2.png" data-holder-rendered="true">
+              </div>
+            </div>
+    
+            <hr class="featurette-divider">
+    
+            <div class="row featurette">
+              <div class="col-md-7">
+                <h2 class="featurette-heading">TECH-INO - 100% Maker! <span class="text-muted"></span></h2> <br>
+                <p class="lead">Queremos você por perto! Siga a gente no Instagram para conhecer nossa estrutura, produtos, bastidores e equipe. <br>
+                    Fique por dentro de novidades, promoções, curiosidades e receba diversos tutoriais do universo Maker.
+                </p>
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto rounded img-thumbnail" data-src="holder.js/500x500/auto" alt="500x500" style="width: 100%; height: 100%;" src="robotica3.png" data-holder-rendered="true">
+              </div>
+            </div>
+    
+            <hr class="featurette-divider">
+    
+            <!-- /END THE FEATURETTES -->
+    
+          </div><!-- /.container -->
+        </main>
+
+        <footer class="text-center text-lg-start bg-white text-muted">
+          <!-- Section: Social media -->
+          <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <!-- Left -->
+            <div class="me-5 d-none d-lg-block">
+              <span>Mantenha-se conectado às nossas redes sociais</span>
+            </div>
+            <!-- Left -->
+        
+            <!-- Right -->
+            <div>
+              <a href="" class="me-4 link-secondary">
+                  <i class="fa-brands fa-facebook"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                  <i class="fa-brands fa-twitter"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                  <i class="fa-brands fa-google"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                  <i class="fa-brands fa-instagram"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                  <i class="fa-brands fa-linkedin"></i>
+              </a>
+              <a href="" class="me-4 link-secondary">
+                  <i class="fa-brands fa-github"></i>
+              </a>
+            </div>
+            <!-- Right -->
+          </section>
+          <!-- Section: Social media -->
+        
+          <!-- Section: Links  -->
+          <section class="">
+            <div class="container text-center text-md-start mt-5">
+              <!-- Grid row -->
+              <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <!-- Content -->
+                  <h6 class="text-uppercase fw-bold mb-4">
+                    <i class="fas fa-gem me-3 text-secondary"></i>TECH-INO
+                  </h6>
+                  <p>
+                    Tudo, em todo lugar, ao mesmo tempo
+                  </p>
+                </div>
+                <!-- Grid column -->
+        
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">
+                    PRODUTOS
+                  </h6>
+                  <p>
+                    <a href="#!" class="text-reset">Termos</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Acessibilidade</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Visite</a>
+                  </p>
+                </div>
+                <!-- Grid column -->
+        
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">
+                    LINKS ÚTEIS
+                  </h6>
+                  <p>
+                    <a href="#!" class="text-reset">Preços</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Configurações</a>
+                  </p>
+                  <p>
+                    <a href="#!" class="text-reset">Pedidos</a>
+                  </p>
+                </div>
+                <!-- Grid column -->
+        
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  <!-- Links -->
+                  <h6 class="text-uppercase fw-bold mb-4">CONTATO</h6>
+                  <p><i class="fas fa-home me-3 text-secondary"></i> Foz do Iguaçu, PR 10012, BR</p>
+                  <p>
+                    <i class="fas fa-envelope me-3 text-secondary"></i>
+                    techino@gmail.com
+                  </p>
+                  <p><i class="fas fa-phone me-3 text-secondary"></i> (45) 99147-6795</p>
+                </div>
+                <!-- Grid column -->
+              </div>
+              <!-- Grid row -->
+            </div>
+          </section>
+          <!-- Section: Links  -->
+        
+          <!-- Copyright -->
+          <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+            © 2021 Copyright:
+            <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+          </div>
+          <!-- Copyright -->
+      </footer>
+      <!-- Footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
